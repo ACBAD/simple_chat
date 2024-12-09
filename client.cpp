0b5816f9208fbad06a67f6bb677ead6b12af6bf1
xd <2080776216@qq.com>
2024-12-09 21:29:29 +0800

    use websocket

--- client.cpp
+++ client.cpp
@@ -1,5 +1,47 @@
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
 #include <iostream>
+#include <thread>
+
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+
+class websocket_client {
+public:
+    websocket_client() {
+        // 初始化 WebSocket 客户端
+        ws_client.init_asio();
+        // 设置回调
+        ws_client.set_open_handler([this](websocketpp::connection_hdl hdl) {
+            std::cout << "Connection opened" << std::endl;
+            connection_hdl = hdl;
+            // 在连接建立后发送一条消息
+            ws_client.send(hdl, "Hello Server", websocketpp::frame::opcode::text);
+        });
+        ws_client.set_close_handler([](websocketpp::connection_hdl hdl) {
+            std::cout << "Connection closed" << std::endl;
+        });
+        ws_client.set_message_handler([](websocketpp::connection_hdl hdl, client::message_ptr msg) {
+            std::cout << "Received: " << msg->get_payload() << std::endl;
+        });
+    }
+    void connect(const std::string& uri) {
+        websocketpp::lib::error_code ec;
+        client::connection_ptr con = ws_client.get_connection(uri, ec);
+        if (ec) {
+            std::cout << "Could not create connection: " << ec.message() << std::endl;
+            return;
+        }
+        ws_client.connect(con);
+        ws_client.run();
+    }
+private:
+    client ws_client;
+    websocketpp::connection_hdl connection_hdl;
+};
+
 using namespace std;
 int main(){
+  websocket_client client;
+  client.connect("ws://localhost:9002");
   return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,7 +1,60 @@
-#include <iostream>
 #include <rapidjson/document.h>
-using namespace std;
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+#include <iostream>
+#include <set>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+typedef server::message_ptr message_ptr;
+
+class websocket_server {
+public:
+    websocket_server() {
+        // 初始化 WebSocket 服务端
+        ws_server.init_asio();
+
+        // 设置回调
+        ws_server.set_open_handler(bind(&websocket_server::on_open, this, std::placeholders::_1));
+        ws_server.set_close_handler(bind(&websocket_server::on_close, this, std::placeholders::_1));
+        ws_server.set_message_handler(bind(&websocket_server::on_message, this, std::placeholders::_1, std::placeholders::_2));
+    }
+
+    void run(uint16_t port) {
+        // 监听指定端口
+        ws_server.listen(port);
+        ws_server.start_accept();
+        ws_server.run();
+    }
+
+private:
+    void on_open(websocketpp::connection_hdl hdl) {
+        // 记录新连接
+        connections.insert(hdl);
+        std::cout << "Connection opened" << std::endl;
+    }
+
+    void on_close(websocketpp::connection_hdl hdl) {
+        // 移除断开的连接
+        connections.erase(hdl);
+        std::cout << "Connection closed" << std::endl;
+    }
+
+    void on_message(websocketpp::connection_hdl hdl, message_ptr msg) {
+        // 回显消息给客户端
+        try {
+            ws_server.send(hdl, msg->get_payload(), msg->get_opcode());
+        } catch (const websocketpp::exception& e) {
+            std::cout << "Message send failed: " << e.what() << std::endl;
+        }
+    }
+
+    server ws_server;
+    std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
+};
+
 int main(){
-  cout<<"Start"<<endl;
+  std::cout<<"Start"<<std::endl;
+  websocket_server server;
+  server.run(9002);
   return 0;
 }
