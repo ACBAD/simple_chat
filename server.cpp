a8030f8b5581214d936e38224f0b480e6eb64a5e
xd <2080776216@qq.com>
2024-12-09 15:30:39 +0800

    delete httplib

--- server.cpp
+++ server.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
 #include <rapidjson/document.h>
-#include <httplib.h>
-#ifdef _WIN32
-#include <Windows.h>
-#endif
 using namespace std;
 int main(){
   cout<<"Start"<<endl;
-  httplib::Server svr;
-  svr.Get("/hi", [](const httplib::Request &, httplib::Response &res){
-    cout<<"Request!"<<endl;
-    res.set_content("Hello World!", "text/plain");
-  });
-  cout<<"Start Listening ..."<<endl;
-  if(!svr.listen("0.0.0.0", 12738))
-    cout<<"Listen thread exit abnomally"<<endl;
   return 0;
 }
